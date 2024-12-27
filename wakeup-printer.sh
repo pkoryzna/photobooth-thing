9f9ad4505a02368f572031012c708ac0f35f5071
Patryk Koryzna <patryk@koryzna.com>
2024-12-27 22:24:20 +0100

    even more fix command to wake up printer

--- wakeup-printer.sh
+++ wakeup-printer.sh
@@ -1,3 +1,4 @@
+cd $HOME/photobooth-thing/
 source $HOME/photobooth-thing/env/bin/activate
 brother_ql --backend pyusb --model QL-720NW --printer usb://0x04f9:0x2044 print --label 62 pixel.png --no-cut
 
