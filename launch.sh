8e966aaad4f6b1b5dc425250d6a930fc5125d4a0
Patryk Koryzna <patryk@koryzna.com>
2024-12-27 12:46:16 +0100

    autostart

---
+++ launch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+
+while true; do
+    (
+    source $HOME/photobooth-thing/env/bin/activate
+    export DISPLAY=:0
+    unclutter
+    python3 -m one_bit_photo.main
+    )
+done
